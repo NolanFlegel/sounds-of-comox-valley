505ab37bb387e84e152771c5db84d46d2a958547
Nolan Flegel <flegel2n@uregina.ca>
2021-12-02 12:10:26 -0800

    pins and sounds added

--- sounds-of-cvrd/src/app/app.component.ts
+++ sounds-of-cvrd/src/app/app.component.ts
@@ -1,4 +1,23 @@
 import { Component } from '@angular/core';
+// import { features } from 'process';
+import * as soundscapes from '../assets/soundscapes.json';
+
+interface SoundScape {
+  soundFile: string,
+  location: {
+    lat: number,
+    lng: number,
+  }
+}
+
+interface MyMarkers {
+  markerPositions: google.maps.LatLngLiteral,
+  markerOptions: google.maps.MarkerOptions,
+}
+
+interface MarkerAudio{
+  audiofile: string
+}
 
 @Component({
   selector: 'app-root',
@@ -7,19 +26,52 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'sounds-of-cvrd';
+  markerArray = [];
   options: google.maps.MapOptions = {
-    center: {lat: 49.6877, lng: -125.01614},
-    zoom: 11
+    mapTypeId:"satellite" ,
+    center: {lat: 49.69684635847244, lng: -124.98160288744413}, 
+    zoom: 13
   };
-  markerOptions: google.maps.MarkerOptions = {}
-  markerPosition:google.maps.LatLngLiteral = {lat: 49.6877, lng: -125.01614};
 
+  public soundScape: SoundScape[] = [];
+  
+  public myMarkers: MyMarkers[] = [];
 
-  helloWorld(){}
+  public markerAudio = new Audio();
+  
+  markerOptions: google.maps.MarkerOptions = {
+    clickable: true,
+    title: "SoundScape Marker"
+  }
+  markerPositions:google.maps.LatLngLiteral[] = [];
+  
+  
+  constructor(){
+    this.soundScape = soundscapes;
 
-  playSoundscape(){
+    // console.log(this.soundScape);
+    
+    for (let i=0; i < this.soundScape.length; i++) {
+      //console.log(this.soundScape[i]);
+      let data: MyMarkers = {
+        markerPositions: this.soundScape[i].location,
+        markerOptions: {
+          clickable: true,
+          title: this.soundScape[i].soundFile,
+          label: (i+1).toString()
+        }
+      }
 
+      this.myMarkers.push(data);
+      //this.markerOptions
+    }
 
+  }
 
+  public playSoundscape(options: any){
+    this.markerAudio.pause();
+    this.markerAudio.src = "../assets/soundscapes/" + options.title;
+    this.markerAudio.load();
+    this.markerAudio.play();
   }
 }
